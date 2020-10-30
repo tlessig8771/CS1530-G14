b97e2809d8b3a9da20958c1b1373ca2d04e726f7
sbansod21 <60163688+sbansod21@users.noreply.github.com>
2020-10-30 12:11:07 -0400

    Add files via upload

---
+++ Food.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Navbar from "./Components/Navbar/Navbar";
+import './Food.css';
+
+function Food() {
+    return (
+    <div className = "Food">
+      <Navbar/>
+      </div>
+    )
+}
---
+++ MenuItems.js
@@ -0,0 +1,28 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Food',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Attractions',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Events',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Log In',
+        url: '#',
+        cName: 'nav-links-mobile'
+    }
+
+]
